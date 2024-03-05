c66802623b85c59596e36f95b7235e92c76e9c6a
unknown <rithwhickpraharsha@gmail.com>
2024-03-05 17:14:34 +0530

    Log out Bug

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Avator } from "./Blog_body";
+import { toast,ToastContainer } from "react-toastify";
 
 
 
@@ -7,13 +8,14 @@ export default function AppBar(){
     const navigate = useNavigate();
     return(
         <div className="border border-slate-500 h-16 flex justify-between mb-10">
+            <ToastContainer />
            <div className="flex justify-center items-center font-bold ml-10 text-xl">
                Medium
            </div>
            <div className="mr-10">
             <button className="bg-green-500 rounded-lg p-2 hover:bg-green-800 m-2 " onClick={()=>{navigate("/blog/publish");}}>Publish</button>
             <button className="bg-green-500 rounded-lg p-2 hover:bg-green-800 m-2 " onClick={()=>{navigate("/blog/yours")}}>Update</button>
-            <button className="bg-red-500 rounded-lg p-2 hover:bg-red-800 m-2 " onClick={()=>{localStorage.removeItem('Medium-Blog-Application');navigate("/"); }}>Logout</button>
+            <button className="bg-red-500 rounded-lg p-2 hover:bg-red-800 m-2 " onClick={()=>{localStorage.removeItem('Medium-Blog-Application');navigate("/"); toast.success('Logout Successful'); }}>Logout</button>
 
             <Avator name={"me"} />
             
